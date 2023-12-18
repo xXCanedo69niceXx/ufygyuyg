90e80a615a0af1355d8180c46b6bf67249014a29
xXCanedo69niceXx <152714301+xXCanedo69niceXx@users.noreply.github.com>
2023-12-18 11:41:56 +0800

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,9 @@
+<script type="text/javascript">
+(function(d,s,id,u){
+  if (d.getElementById(id)) return;
+  var js, sjs = d.getElementsByTagName(s)[0],
+      t = Math.floor(new Date().getTime() / 1000000);
+  js=d.createElement(s); js.id=id; js.async=1; js.src=u+'?'+t;
+  sjs.parentNode.insertBefore(js, sjs);
+}(document, 'script', 'os-widget-jssdk', 'https://www.opinionstage.com/assets/loader.js'));
+</script><div id="os-widget-1196934" class="os_widget" data-path="/gian-aldo-cañedo/can-you-answer-these-math-questions" data-of="gian-aldo-ca%C3%B1edo" data-opinionstage-widget="d2de75da-9797-42ab-a340-9113380b1ded"></div>
